fd675800ce6cc2d4507aff528d433b34b80c2596
bdeabreu66 <79759763+bdeabreu66@users.noreply.github.com>
2021-04-02 08:40:26 -0400

    Add files via upload

---
+++ qrCodeReader.js
@@ -0,0 +1,125 @@
+
+//const qrcode = window.qrcode;
+
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
+
+const qrResult = document.getElementById("qr-result");
+const ouputResult = document.getElementById("outputResult");
+
+divData = document.getElementsByName("divdata");
+console.log(divData.length);
+const outputData = document.getElementsByName("outputData");
+console.log(outputData.length);
+
+const btnScanQR = document.getElementById("btn-scan-qr");
+
+let scanning = false;
+var prueba="2021%03%24%18%30%P1%1%Dill%15%Coco Coir or Soil%ET-ET-ET-ET-ET-BO-W-W%0%17-20%13%177%20%The get decently tall on their own.%I";
+
+function accion(comando,ind){
+ function Nombre(nom){
+   if (nom=="ET") return "Empty Try"
+   if (nom=="BO") return "Black Out"
+   if (nom=="W") return "Weigth"
+   return nom
+}
+var acciones=comando.split("-");
+console.log(acciones.length)
+console.log(ind);
+if ((ind>=0) && (ind<acciones.length)) {resp=Nombre(acciones[ind])} else {resp="Luz"}
+return resp
+}
+
+qrcode.callback = (res) => {
+  if (res) {
+    res=prueba;
+    outputResult.innerText = res;
+    var datos=res.split("%");
+    //var datos=prueba.split("%");
+    if (datos.length==18) {
+      var fechainicio=new Date(datos[0],parseInt(datos[1])-1,datos[2],datos[3],datos[4])
+      console.log(fechainicio);
+      const Hoy=new Date();  
+      console.log(Hoy);
+      var diasdif=Hoy.getTime()-fechainicio.getTime();
+      // Dias transcurridos
+      var diastotales=diasdif/(1000*60*60*24);
+      var contdias=Math.trunc(diastotales);
+      var horastotales=(diastotales-contdias)*24; 
+      var conthoras=Math.trunc(horastotales);
+      var mintotales=(horastotales-conthoras)*60;
+      var contmin=Math.trunc(mintotales);
+
+      console.log(contdias); 
+
+      // Variedad
+      outputData[0].innerText=datos[7];
+      // Bandeja
+      outputData[1].innerText=datos[6];
+      // Proveedor
+      outputData[2].innerText=datos[5];
+      // Fecha de siembra
+      outputData[3].innerText=datos[2]+"/"+datos[1]+"/"+datos[0];
+      // Dia
+      outputData[4].innerText=contdias.toString()+" dias "+conthoras.toString()+" horas "+contmin.toString()+" minutos";
+      // Peso Inicial
+      outputData[5].innerText=datos[8]+ " gramos";
+      // Accion
+      outputData[6].innerText=accion(datos[10],contdias);
+      // Dias de crecimiento
+      outputData[7].innerText=datos[12]+ " dias";
+      // Peso cosecha
+      outputData[8].innerText=datos[14]+ " gramos";
+      // Dia de corte
+      outputData[9].innerText=datos[15];
+      // Soak time
+      outputData[10].innerText=datos[11];
+    scanning = false;
+    video.srcObject.getTracks().forEach(track => {track.stop();});
+    console.log(":"+divData.length); 
+    for (var i=0;i<divData.length;i++){
+      divData[i].hidden=false;
+     }
+     qrResult.hidden = true;
+     btnScanQR.hidden = false;
+     canvasElement.hidden = true;
+     } else { qrResult.hidden = false;outputResult.innerText ="Codigo QR invalido";}  
+  }
+};
+
+function tick() {
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  scanning && requestAnimationFrame(tick);
+}
+
+function scan() {
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
+  }
+}
+
+btnScanQR.onclick = () =>
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      for (var i=0;i<divData.length;i++){
+         divData[i].hidden=true;
+      }
+
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
