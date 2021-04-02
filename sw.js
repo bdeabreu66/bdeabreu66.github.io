262da756610331fb258cc9b461f76629706f7ddf
bdeabreu66 <79759763+bdeabreu66@users.noreply.github.com>
2021-04-02 09:54:09 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -33,8 +33,7 @@ self.addEventListener(‘fetch’, function(e){
 		caches.match(e.request).then(function(response){
 			return response || fetch(e.request);
                             })
-                            );
-                    );
+                      );
 });
 
 
