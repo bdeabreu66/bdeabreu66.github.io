294226c0a2e2f45722ffd4344e27bc51179da636
bdeabreu66 <79759763+bdeabreu66@users.noreply.github.com>
2021-04-08 14:56:45 -0400

    Add files via upload

---
+++ mgreen/sw.js
@@ -0,0 +1,40 @@
+
+const currentCache = 'cache-v1.0';
+
+const files =
+[
+    '/index.html',
+    '/style.css',
+    '/qrcode.js',
+    '/qrCodeReader.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache).then(cache => {
+      return cache.addAll(files);
+                               })
+  );
+});
+
+// Borra las cache anteriores
+self.addEventListener('activate', event => {
+  event.waitUntil(
+       caches.keys().then(cacheNames => Promise.all(
+       cacheNames.filter(cacheName => {
+           return cacheName !== currentCache
+         }).map(cacheName => caches.delete(cacheName))
+        ))
+  );
+});
+
+// carga de los cache si existen
+self.addEventListener(‘fetch’, function(e){
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+			return response || fetch(e.request);
+                            })
+                            );
+                    );
+});
+
