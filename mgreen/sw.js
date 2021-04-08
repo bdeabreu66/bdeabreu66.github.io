b946bfe57f0a074f0c02445cb821d4d160afa1e8
bdeabreu66 <79759763+bdeabreu66@users.noreply.github.com>
2021-04-08 16:31:19 -0400

    Add files via upload

--- mgreen/sw.js
+++ mgreen/sw.js
@@ -1,11 +1,11 @@
-
+//
 const currentCache = 'cache-v1.0';
-
 const files =
 [
-    '/index.html',
-    '/style.css',
- ];
+    'index.html',
+    'styles.css',
+    'icondef.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -16,6 +16,7 @@ self.addEventListener('install', event => {
 });
 
 // Borra las cache anteriores
+/*
 self.addEventListener('activate', event => {
   event.waitUntil(
        caches.keys().then(cacheNames => Promise.all(
@@ -25,14 +26,27 @@ self.addEventListener('activate', event => {
         ))
   );
 });
-
+*/
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys().then(function(names) {
+        return Promise.all(
+            names.filter(function(name) {
+                return name !== currentCache;
+            }).map(function(name) {
+                return caches.delete(name);
+            })
+        );
+    }).then(function() {
+        return self.clients.claim();
+    }));
+});
 // carga de los cache si existen
-self.addEventListener(‘fetch’, function(e){
+self.addEventListener('fetch', function(e){
 	e.respondWith(
 		caches.match(e.request).then(function(response){
 			return response || fetch(e.request);
                             })
-                            );
-                    );
+                      );
 });
+
 
