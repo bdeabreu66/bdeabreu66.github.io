b84e1dcdaa728fec2ce428cfca60a03f991bcfab
bdeabreu66 <79759763+bdeabreu66@users.noreply.github.com>
2021-06-16 11:38:31 -0400

    Add files via upload

---
+++ precios/sw.js
@@ -0,0 +1,51 @@
+//
+const currentCache = 'cache-v3.0';
+const files =
+[
+    'index.html',
+    'icondef.png'
+ ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache).then(cache => {
+      return cache.addAll(files);
+                               })
+  );
+});
+
+// Borra las cache anteriores
+/*
+self.addEventListener('activate', event => {
+  event.waitUntil(
+       caches.keys().then(cacheNames => Promise.all(
+       cacheNames.filter(cacheName => {
+           return cacheName !== currentCache
+         }).map(cacheName => caches.delete(cacheName))
+        ))
+  );
+});
+*/
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys().then(function(names) {
+        return Promise.all(
+            names.filter(function(name) {
+                return name !== currentCache;
+            }).map(function(name) {
+                return caches.delete(name);
+            })
+        );
+    }).then(function() {
+        return self.clients.claim();
+    }));
+});
+// carga de los cache si existen
+self.addEventListener('fetch', function(e){
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+			return response || fetch(e.request);
+                            })
+                      );
+});
+
+
