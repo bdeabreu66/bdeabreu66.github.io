d33ffcfa01fa78afab48a03aaef397064e826712
bdeabreu66 <79759763+bdeabreu66@users.noreply.github.com>
2023-03-11 14:36:48 -0400

    Add files via upload

--- banco/sw.js
+++ banco/sw.js
@@ -1,5 +1,5 @@
 // 25-02-2023
-const currentCache = 'BNC_banco-cache-v1.4';
+const currentCache = 'BNC_banco_cache_v1.5';
 const files =
 [
     'index.html',
@@ -16,17 +16,7 @@ self.addEventListener('install', event => {
 });
 
 // Borra las cache anteriores
-/*
-self.addEventListener('activate', event => {
-  event.waitUntil(
-       caches.keys().then(cacheNames => Promise.all(
-       cacheNames.filter(cacheName => {
-           return cacheName !== currentCache
-         }).map(cacheName => caches.delete(cacheName))
-        ))
-  );
-});
-*/
+
 self.addEventListener('activate', function(event) {
     event.waitUntil(caches.keys().then(function(names) {
         return Promise.all(
@@ -51,25 +41,3 @@ self.addEventListener('fetch', function(e){
                       );
 });
 
-/*
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    (async () => {
-      const cache = await caches.open(currentCache);
-      await cache.addAll(contentToCache);
-    })()
-  );
-});
-
-async function detectSWUpdate() {
-  const registration = await navigator.serviceWorker.ready;
-
-  registration.addEventListener("updatefound", event => {
-    const newSW = registration.installing;
-    newSW.addEventListener("statechange", event => {
-      if (newSW.state == "installed") {
-         // New service worker is installed, but waiting activation
-      }
-    });
-  })
-*/
