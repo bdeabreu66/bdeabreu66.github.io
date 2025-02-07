e855d5b5ee42e9329cd6176dbab2535ed815ac5a
bdeabreu66 <79759763+bdeabreu66@users.noreply.github.com>
2025-02-07 19:34:18 -0400

    Add files via upload

---
+++ ecoinf/higado.js
@@ -0,0 +1,78 @@
+function showHigado(){
+divA.style.visibility="hidden";
+divH=document.getElementById('Higado');
+divH.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Higado','showAbdominal','divH');
+texto+=' <p class="menuc" onclick="showHigDet();">General</p>';
+texto+=' <p class="menuc" onclick="showFocal(\'Higado\',\'showHigado\',divH);">Focal</p>';
+texto+=' <p class="menuc">Dopler</p>';
+texto+=' <p class="menuc" onclick="showHigTam();">Medidas</p>';
+divH.innerHTML=texto;
+divH.style.visibility="visible";
+if (typeof(Examen.higado)==='undefined')  {Examen.higado=new Object();}
+}
+// Actualiza Higado
+function setHigado(){
+ try {Examen.higado.HigTam1=document.getElementById('HigTam1').value;} catch {console.log("error")};
+ try {Examen.higado.HigTam2=document.getElementById('HigTam2').value;} catch {console.log("error")};
+ try {Examen.higado.HigTam3=document.getElementById('HigTam3').value;} catch {console.log("error")};
+ try {Examen.higado.HigTam4=document.getElementById('HigTam4').value;} catch {console.log("error")};
+ try {
+  if (document.getElementById("Higborde1").checked) {Examen.higado.borde="liso"}
+  if (document.getElementById("Higborde2").checked) {Examen.higado.borde="no_liso"}
+ } catch {console.log("error")}
+try {
+  if (document.getElementById("HigCap").checked) {Examen.higado.capsula="checked";}
+   else {Examen.higado.capsula="";}
+ } catch {console.log("error")}
+try {
+  if (document.getElementById("HigDia").checked) {Examen.higado.dia="checked";}
+   else {Examen.higado.dia="";}
+ } catch {console.log("error")}
+
+
+}
+// Medidas del higado
+function showHigTam(){
+divH.style.visibility="hidden";
+divHT=document.getElementById('HigTam');
+divHT.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Higado','showHigado','divHT');
+if (typeof(Examen.higado.HigTam1)==='undefined') 
+ {Examen.higado.HigTam1=0;v1=0} else {v1=Examen.higado.HigTam1}
+if (typeof(Examen.higado.HigTam2)==='undefined') 
+ {Examen.higado.HigTam2=0;v2=0} else {v2=Examen.higado.HigTam2}
+if (typeof(Examen.higado.HigTam3)==='undefined') 
+ {Examen.higado.HigTam3=0;v3=0} else {v3=Examen.higado.HigTam3}
+if (typeof(Examen.higado.HigTam4)==='undefined') 
+ {Examen.higado.HigTam4=0;v4=0} else {v4=Examen.higado.HigTam4}
+
+texto+=' <label for="HigTam1">L&oacute;bulo derecho (cm):</label><input onchange="setHigado()" type="number" id="HigTam1" value='+v1+'><br>';
+texto+=' <label for="HigTam2">L&oacute;bulo izquierdo (cm):</label><input onchange="setHigado()" type="number" id="HigTam2" value='+v2+'><br>';
+texto+=' <label for="HigTam3">L&oacute;bulo caudado (cm):</label><input onchange="setHigado()" type="number" id="HigTam3" value='+v3+'><br>';
+texto+=' <label for="HigTam4">Di&aacute;metro Vena Porta (cm):</label><input onchange="setHigado()" type="number" id="HigTam4" value='+v4+'><br>';
+
+divHT.innerHTML=texto;
+divHT.style.visibility="visible";
+}
+function showHigDet(){
+divH.style.visibility="hidden";
+divHD=document.getElementById('HigDet');
+divHD.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Higado','showHigado','divHD');
+if (typeof(Examen.higado.borde)==='undefined') {Examen.higado.borde="liso";v1="checked";v2=""} 
+if (Examen.higado.borde=="liso") {v1="checked";v2="";} else {v1="";v2="checked"}
+if (typeof(Examen.higado.capsula)==='undefined') 
+ {Examen.higado.capsula="checked";v3="checked"} else {v3=Examen.higado.capsula}
+if (typeof(Examen.higado.dia)==='undefined') 
+ {Examen.higado.dia="checked";v4="checked"} else {v4=Examen.higado.dia}
+
+texto+='Borde:<br><label for="HigBorde1">&nbsp &nbsp Liso</label><input type="radio" id="HigBorde1" name="HigBorde" '+ v1 +' onchange="setHigado()"><br>';
+texto+='<label for="HigBorde2">&nbsp &nbsp Festoneado</label><input type="radio" id="HigBorde2" name="HigBorde" '+ v2 +' onchange="setHigado()"><br>';
+texto+='<label for="HigCap">Linea de la c&aacute;psula: </label><input type="checkbox" id="HigCap" '+ v3 +' onchange="setHigado()"><br>';
+texto+='<label for="HigDia">Linea diafragm&aacute;tica: </label><input type="checkbox" id="HigDia" '+ v4 +' onchange="setHigado()"><br>';
+
+
+divHD.innerHTML=texto;
+divHD.style.visibility="visible";
+}
---
+++ ecoinf/ovarios.js
@@ -0,0 +1,101 @@
+function showOvD(){
+divPelvis.style.visibility="hidden";
+divOvD=document.getElementById('OvD');
+divOvD.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Ovario Derecho','showPelvico','divOvD');
+texto+=' <p class="menuc" onclick="showOvDDet();">General</p>';
+texto+=' <p class="menuc">Focal</p>';
+texto+=' <p class="menuc" onclick="showOvDTam();">Medidas</p>';
+divOvD.innerHTML=texto;
+divOvD.style.visibility="visible";
+if (typeof(Examen.ovd)==='undefined')  {Examen.ovd=new Object();}
+
+}
+
+function setOvD(){
+try { Examen.ovd.OvDTam1=document.getElementById('OvDTam1').value;} catch {console.log('Error')}
+
+try {
+
+if (document.getElementById("OvDVis").checked)  {Examen.ovd.OvDVis='checked'} else {Examen.ovd.OvDVis=''}
+} catch {console.log('Error')}
+}
+
+function showOvDTam(){
+divOvD.style.visibility="hidden";
+divOvDT=document.getElementById('OvDTam');
+divOvDT.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Ovario derecho','showOvD','divOvDT');
+if (typeof(Examen.ovd.OvDTam1)==='undefined') 
+ {Examen.ovd.OvDTam1=0;v1=0} else {v1=Examen.ovd.OvDTam1}
+
+texto+=' <label for="OvDTam1">A-P(cm):</label><input onchange="setOvD()" type="number" id="OvDTam1" value="'+v1+'"><br>';
+
+divOvDT.innerHTML=texto;
+divOvDT.style.visibility="visible";
+}
+
+function showOvDDet(){
+divOvD.style.visibility="hidden";
+divOvDD=document.getElementById('OvDDet');
+divOvDD.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Ovario derecho','showOvD','divOvDD');
+if (typeof(Examen.ovd.OvDVis)==='undefined') 
+ {Examen.ovd.OvDVis="checked";v1="checked"} else {v1=Examen.ovd.OvDVis}
+
+texto+='<label for="OvDVis">Visible</label><input type="checkbox" id="OvDVis" '+ v1 +' onchange="setOvD()"><br>';
+
+divOvDD.innerHTML=texto;
+divOvDD.style.visibility="visible";
+}
+
+function showOvI(){
+divPelvis.style.visibility="hidden";
+divOvI=document.getElementById('OvI');
+divOvI.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Ovario izquierdo','showPelvico','divOvI');
+texto+=' <p class="menuc" onclick="showOvIDet();">General</p>';
+texto+=' <p class="menuc">Focal</p>';
+texto+=' <p class="menuc" onclick="showOvITam();">Medidas</p>';
+divOvI.innerHTML=texto;
+divOvI.style.visibility="visible";
+if (typeof(Examen.ovi)==='undefined')  {Examen.ovi=new Object();}
+
+}
+
+function setOvI(){
+try { Examen.ovi.OvITam1=document.getElementById('OvITam1').value;} catch {console.log('Error')}
+
+try {
+
+if (document.getElementById("OvIVis").checked)  {Examen.ovi.OvIVis='checked'} else {Examen.ovi.OvIVis=''}
+} catch {console.log('Error')}
+}
+
+function showOvITam(){
+divOvI.style.visibility="hidden";
+divOvIT=document.getElementById('OvITam');
+divOvIT.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Ovario izquierdo','showOvI','divOvIT');
+if (typeof(Examen.ovi.OvITam1)==='undefined') 
+ {Examen.ovi.OvITam1=0;v1=0} else {v1=Examen.ovi.OvITam1}
+
+texto+=' <label for="OvITam1">A-P(cm):</label><input onchange="setOvI()" type="number" id="OvITam1" value="'+v1+'"><br>';
+
+divOvIT.innerHTML=texto;
+divOvIT.style.visibility="visible";
+}
+
+function showOvIDet(){
+divOvI.style.visibility="hidden";
+divOvID=document.getElementById('OvIDet');
+divOvID.style="position:fixed;top:25%;left:0%;height:60%;width:100%;overflow-y:auto;"
+var texto=Titulo('Ovario izquierdo','showOvI','divOvID');
+if (typeof(Examen.ovi.OvIVis)==='undefined') 
+ {Examen.ovi.OvIVis="checked";v1="checked"} else {v1=Examen.ovi.OvIVis}
+
+texto+='<label for="OvIVis">Visible</label><input type="checkbox" id="OvIVis" '+ v1 +' onchange="setOvI()"><br>';
+
+divOvID.innerHTML=texto;
+divOvID.style.visibility="visible";
+}
