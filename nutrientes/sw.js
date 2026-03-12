cc3068159f92e1eb90e6f6ed39761b409a681b1b
bdeabreu66 <79759763+bdeabreu66@users.noreply.github.com>
2026-03-12 18:45:05 -0400

    Add files via upload

---
+++ nutrientes/sw.js
@@ -0,0 +1,47 @@
+const currentCache = 'FACPOLAR_cache_v5.5';
+const files =
+[
+    'fecha.txt',
+    'manifest.json',
+    'barras.js',
+    'index.html',
+    'icondef.png',
+    'arbol.js',
+    'FileSaver.js',
+    'selectFile.js'
+ ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache).then(cache => {
+      return cache.addAll(files);
+                               })
+  );
+});
+
+// Borra las cache anteriores
+
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys().then(function(names) {
+        return Promise.all(
+            names.filter(function(name) {
+                return name !== currentCache;
+            }).map(function(name) {
+                return caches.delete(name);
+            })
+        );
+    }).then(function() {
+        return self.clients.claim();
+    }));
+});
+
+// carga de los cache si existen
+self.addEventListener('fetch', function(e){
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+			return response || fetch(e.request);
+                            })
+                      );
+});
+
